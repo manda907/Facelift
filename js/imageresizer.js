8d729827daac6df72e71a238403d545882313954
David Bjerremose <dab@davidbjerremose.com>
2012-04-17 22:22:39 +0200

    Added all the files for now.

---
+++ js/imageresizer.js
@@ -0,0 +1,7 @@
+function imageResize(size, imagesize) {
+	var size = (size[0] > size[1])?size[0]:size[1];
+	var imagesize = (imagesize[0] > size[1])?imagesize[0]:imagesize[1];
+	if (size > imageresize) {
+		return size/imagesize*100;
+	}
+}
