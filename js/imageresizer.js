8b5083a214019c5e2147adb8efd1c489159b944b
David Bjerremose <dab@davidbjerremose.com>
2012-04-26 23:54:59 +0200

    Trying to reconstruct some messy code

--- js/imageresizer.js
+++
@@ -1,7 +0,0 @@
-function imageResize(size, imagesize) {
-	var size = (size[0] > size[1])?size[0]:size[1];
-	var imagesize = (imagesize[0] > size[1])?imagesize[0]:imagesize[1];
-	if (size > imageresize) {
-		return size/imagesize*100;
-	}
-}
