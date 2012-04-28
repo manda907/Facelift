0e284a015eee58055f4b6206524a3d841a7dd651
DaBs <dab@davidbjerremose.com>
2012-04-28 11:03:58 +0300

    Update js/imagefit.js

--- js/imagefit.js
+++ js/imagefit.js
@@ -1,32 +1,41 @@
 (function($) {
-  $.fn.imagefit = function(){
-    var bgImg = $(this);		
- 
-    function resizeImg() {
-      var imgwidth = bgImg.width();
-      var imgheight = bgImg.height();
- 
-      var winwidth = $(this).parent().width();
-      var winheight = $(this).parent().height();
- 
-      var widthratio = winwidth / imgwidth;
-      var heightratio = winheight / imgheight;
- 
-      var widthdiff = heightratio * imgwidth;
-      var heightdiff = widthratio * imgheight;
- 
-      if(heightdiff>winheight) {
-        bgImg.css({
-          width: winwidth+'px',
-          height: heightdiff+'px'
-        });
-      } else {
-        bgImg.css({
-          width: widthdiff+'px',
-          height: winheight+'px'
-        });		
-      }
-    } 
-    resizeImg(); 
-  };
-})(jQuery)
+  $.fn.imagefit = function(options) {
+		var fit = {
+			all : function(imgs){
+				imgs.each(function(){
+					fit.one(this);
+					})
+				},
+			one : function(img){
+				$(img)
+					.width('100%').each(function()
+					{
+						$(this).height(Math.round(
+							$(this).attr('startheight')*($(this).width()/$(this).attr('startwidth')))
+						);
+					})
+				}
+		};
+		
+		this.each(function(){
+				var container = this;
+				
+				// store list of contained images (excluding those in tables)
+				var imgs = $('img', container).not($("table img"));
+				
+				// store initial dimensions on each image 
+				imgs.each(function(){
+					$(this).attr('startwidth', $(this).width())
+						.attr('startheight', $(this).height())
+						.css('max-width', $(this).attr('startwidth')+"px");
+				
+					fit.one(this);
+				});
+				// Re-adjust when window width is changed
+				$(window).bind('resize', function(){
+					fit.all(imgs);
+				});
+			});
+		return this;
+	};
+})(jQuery);
