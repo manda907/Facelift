43671074eb00d147ee391d6f591642dc0a090a8b
DaBs <dab@davidbjerremose.com>
2012-04-28 11:47:42 +0300

    Update js/imagefit.js

--- js/imagefit.js
+++ js/imagefit.js
@@ -1,29 +1,20 @@
 (function($) {
 	$.fn.imagefit = function(options) {
-		var fit = {
-			one : function(img, container){
-				alert($(img).width());
-				alert($(container).width());
-				if ($(img).width < $(container).width) {
-					$(img).width('100%').height('auto');
-				} else if ($(img).height < $(container).height) {
-					$(img).height('100%').width('auto');
-				}
+		var $imgContainer = $(this);
+		var $newsImg = $(this + " img");
+		$imgContainer.css("width", $imgContainer.parent().width());
+		if ($newsImg.length > 0) {
+			if ($newsImg.width() < $imgContainer.width()) {
+				$imgContainer.width($newsImg.width());
 			}
-		};
-		
-		this.each(function(){
-				var container = this;
-				var con = $(this[0]);
-				
-				// store list of contained images (excluding those in tables)
-				var imgs = $('img', container).not($("table img"));
-				
-				// store initial dimensions on each image 
-				imgs.each(function(){
-					fit.one(this, con);
-				});
+			if ($newsImg.height() < $imgContainer.height()) {
+				$imgContainer.height($newsImg.height());
+			}
+			$newsImg.imgscale({ 
+				parent : $(this), 
+				center: "true",
+				scale: "fill"
 			});
-		return this;
+		}
 	};
 })(jQuery);
